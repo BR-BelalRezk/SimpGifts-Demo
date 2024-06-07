f225d9f71b7f122bcc23c50426af040df825296b
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-06-07 15:39:25 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,4 @@
-import { Route, Routes } from "react-router-dom";
 import Form from "../components/shared/Form";
-import Profile from "../components/shared/Profile";
 
 export default function App() {
   return <Form />;
